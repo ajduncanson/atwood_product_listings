b74eea2129db12a5c05ed3fa7a1b23c15037b08c
ajduncanson <kenric.nguyen@mozo.com.au>
2024-01-24 14:50:43 +1100

    daily click config now live

--- code/daily_click_reports_config.py
+++ code/daily_click_reports_config.py
@@ -21,7 +21,7 @@ cred_info['gsheets'] = [
     {'provider': 'Lendi',
      'content': {'HomeLoanBrokerReferral': {'cols': ['Clicks', 'Spend'], 'product_level': 'regex'}
                  },
-     'gsheets_key': '1Lw-p5ocXlLTw8gv9Nw0gXhWq2k_OhWCY6qoTtVjWJo0',
+     'gsheets_key': '1fMYGVXwxUKmbvW6gVx50l7cvSNPtW17CEb1h6S0MGU0',
      'grouping': {'regex': 'TTAB', 
                   'label': 'Talk To A Broker', 
                   'other_label': 'Other placements'}
@@ -30,52 +30,52 @@ cred_info['gsheets'] = [
     {'provider': 'Harmoney',
      'content': {'PersonalLoan': {'cols': ['Clicks', 'Spend'], 'product_level': 'product'}
                  },
-     'gsheets_key': '1Lw-p5ocXlLTw8gv9Nw0gXhWq2k_OhWCY6qoTtVjWJo0'},
+     'gsheets_key': '1QVjVDXhTx-9No8WR3gItG_GpVKvmbD35AcMdryjyANo'},
 
     {'provider': 'Ubank',
      'content': {'HomeLoan': {'cols': ['Clicks', 'Spend'], 'product_level': 'type'},
                  'SavingsAccount': {'cols': ['Clicks', 'Spend'], 'product_level': 'type'},
                  'BankAccount': {'cols': ['Clicks', 'Spend'], 'product_level': 'type'}
                  },
-     'gsheets_key': '1Lw-p5ocXlLTw8gv9Nw0gXhWq2k_OhWCY6qoTtVjWJo0'},
+     'gsheets_key': '1f4qC3I9-TWnFA41ZjslWtYw45COX_Oim5MABYRKrjNo'},
 
-    {'provider': 'Bendigo Bank',
-     'content': {'HomeLoan': {'cols': ['Clicks', 'Apps', 'Spend'], 'product_level': 'type'}
-                 },
-     'gsheets_key': '1Lw-p5ocXlLTw8gv9Nw0gXhWq2k_OhWCY6qoTtVjWJo0'},
+    # {'provider': 'Bendigo Bank',
+    #  'content': {'HomeLoan': {'cols': ['Clicks', 'Apps', 'Spend'], 'product_level': 'type'}
+    #              },
+    #  'gsheets_key': '1Lw-p5ocXlLTw8gv9Nw0gXhWq2k_OhWCY6qoTtVjWJo0'},
 
     {'provider': 'Bendigo Bank',
      'content': {'BankAccount': {'cols': ['Clicks', 'Spend'], 'product_level': 'type'},
                  'SavingsAccount': {'cols': ['Clicks', 'Spend'], 'product_level': 'type'}
                  },
-     'gsheets_key': '1Lw-p5ocXlLTw8gv9Nw0gXhWq2k_OhWCY6qoTtVjWJo0'},
+     'gsheets_key': '19xiTo-f5z-IVLRKQwFuQkm39TMr6x5nt_wnvCUOS4Qo'},
 
     {'provider': 'OurMoneyMarket',
      'content': {'PersonalLoan': {'cols': ['Clicks', 'Spend'], 'product_level': 'type'},
                  'CarLoan': {'cols': ['Clicks', 'Spend'], 'product_level': 'type'},
                  },
-     'gsheets_key': '1Lw-p5ocXlLTw8gv9Nw0gXhWq2k_OhWCY6qoTtVjWJo0'},
+     'gsheets_key': '1IIqNKIm7J9izhmSHWlMKWq06rWC-84rXULoJgb1YTWg'},
 
      {'provider': 'honey',
-     'content': {'HomeInsurance': {'cols': ['Clicks', 'Spend'], 'product_level': 'type'}
+     'content': {'HomeInsurance': {'cols': ['Clicks'], 'product_level': 'type'}
                  },
-     'gsheets_key': '1Lw-p5ocXlLTw8gv9Nw0gXhWq2k_OhWCY6qoTtVjWJo0'},
+     'gsheets_key': '1nhz0sp-y3tp8caPJB8CF-RBdC1iAmJCAHe5Af9sxe_s'},
 
      {'provider': 'AMP Bank',
      'content': {'BankAccount': {'cols': ['Clicks', 'Spend'], 'product_level': 'type'}
                  },
-     'gsheets_key': '1Lw-p5ocXlLTw8gv9Nw0gXhWq2k_OhWCY6qoTtVjWJo0'},
+     'gsheets_key': '1zcWeCHy8q8EwcAb492pEsJNL8cSoVyxjbsd-63hjB08'},
 
      {'provider': 'Freely',
-     'content': {'TravelInsurance': {'cols': ['Clicks', 'Spend'], 'product_level': 'type'}
+     'content': {'TravelInsurance': {'cols': ['Clicks'], 'product_level': 'type'}
                  },
-     'gsheets_key': '1Lw-p5ocXlLTw8gv9Nw0gXhWq2k_OhWCY6qoTtVjWJo0'},
+     'gsheets_key': '1rL_8mIOvqAjGrEaQFbiWCxFoikhdQZGQHxJ0d5dca0g'},
 
      {'provider': 'Virgin Money',
      'content': {'CarInsurance': {'cols': ['Clicks'], 'product_level': 'type'},
                  'HomeInsurance': {'cols': ['Clicks'], 'product_level': 'type'}
                  },
-     'gsheets_key': '1Lw-p5ocXlLTw8gv9Nw0gXhWq2k_OhWCY6qoTtVjWJo0'}  
+     'gsheets_key': '1gIK0X_998BWh5OGMdR481hXsfwL4VCoo1eyAe1qeJUE'}  
 
 ]  
 
